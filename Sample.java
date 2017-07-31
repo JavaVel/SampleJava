6517bf6e2653f009438e4e23eff9a49a81a3af49
unknown <velk1048@gmail.com>
2017-07-31 07:51:04 +0530

    Added logic

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample {
 	public static void main(String[] args) {
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 0; i <= 200; i++) {
 			System.out.println("Helllo:" + i);
 		}
 
